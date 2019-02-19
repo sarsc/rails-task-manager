9871056f1db757d54d545903a1d7a44c32d5c58d
Sara Scaini <sa.scaini@gmail.com>
2019-02-19 16:56:08 +0000

    implemented crud for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,41 @@
 class TasksController < ApplicationController
+def index
+  @tasks = Task.all
+end
+def show
+  @tasks = Task.find(params[:id])
+  # raise
+end
+
+def new
+  @task = Task.new
+end
+
+def create
+  Task.create(tasks_params)
+  redirect_to tasks_path
+  # redirect_to tasks_path(@tasks)
+end
+
+def edit
+  @task = Task.find(params[:id])
+end
+
+def update
+  @task = Task.find(params[:id])
+  @task.update(tasks_params)
+  redirect_to tasks_path
+end
+
+def destroy
+   @task = Task.find(params[:id])
+   @task.destroy
+   redirect_to tasks_path
+end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_115550) do
+ActiveRecord::Schema.define(version: 2019_02_19_122439) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.text "detail"
+    t.text "details"
     t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
